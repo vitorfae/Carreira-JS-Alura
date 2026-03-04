ac372ec17d508f53e79ff7fcef41f4678fabfd7e
unknown <vitorfaeulrich0@gmail.com>
2026-03-03 23:13:31 -0300

    Iniciando estudos de criação de API Alura

---
+++ script.js
@@ -0,0 +1,12 @@
+idade = 21
+
+console.log('minha idade atual: ')
+console.log(idade)
+
+// ou para na mesma linha
+
+console.log('MInha idade atual: ', idade)
+
+idade = 22
+
+console.log('Minha idade apos o aniversario: ', idade)
