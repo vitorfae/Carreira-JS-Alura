b2ab9f6dfc6ef486a68168f33b7ad5d505552513
unknown <vitorfaeulrich0@gmail.com>
2026-03-23 22:41:12 -0300

    Operadores matematicos e de comparacao

---
+++ operadoresComparacao.js
@@ -0,0 +1,33 @@
+const idadeVinny = 36
+const minhaIdade = 21
+
+
+// Operador de igualdade ==
+const saoIguaisComparacao = idadeVinny == minhaIdade
+console.log(`Idade e igual? ${saoIguaisComparacao}`)
+
+//Operador de maior que >
+const comparacaoIdadeMaior = idadeVinny > minhaIdade
+console.log(`Idade de Vinny e maior que a minha? ${comparacaoIdadeMaior}`)
+
+//Operador de menor que
+const comparacaoIdadeMenor = idadeVinny < minhaIdade
+console.log(`Idade de Vinny e menor que a minha? ${comparacaoIdadeMenor}`)
+
+const idade = 21
+const idadeIrmao = 16
+
+const souMaiorDeIdade = idade >= 18
+const irmaoMaiorDeIdade = idadeIrmao >= 18
+
+console.log(`Sou maior de idade? ${souMaiorDeIdade}`)
+console.log(`Meu irmao e maior de idade? ${irmaoMaiorDeIdade}`)
+
+const numerico = 20
+const alfa     ="20"
+
+const igualdade = numerico == alfa
+console.log(`Numerico e igual a alfa: ${igualdade}`)
+
+const estritamenteIgual = numerico === alfa
+console.log(`Numerico e estritamente igual a alfa: ${igualdade}`)
---
+++ operadoresMatematicos.js
@@ -0,0 +1,45 @@
+const numeroUm   = 4
+const numeroDois = 8
+
+const subtracao = numeroUm - numeroDois
+
+console.log(`Subtração: ${subtracao}`)
+
+const soma = numeroUm + numeroDois
+
+console.log(`Soma: ${soma}`)
+
+const divisao = numeroUm / numeroDois
+
+console.log(`Divisao: ${divisao}`)
+
+const multiplicacao = numeroUm * numeroDois
+
+console.log(`Multiplicacao: ${multiplicacao}`)
+
+contaComplexa = numeroUm + numeroDois * numeroUm
+
+console.log(`Conta complexa: ${contaComplexa}`)
+
+const resto = 12%5
+console.log(`Resto da divisao: ${resto}`)
+
+const potencia =  2 ** 3
+console.log(`Potencia ${potencia}`)
+
+let contador = 5
+contador++ /* ++ adiciona mais um no contador*/
+console.log(`Incremento: ${contador}`)
+
+contador-- /* -- remove um no contador*/
+console.log(`Decremento: ${contador}`)
+
+let numeroTres = 2
+
+// numeroTres += 2
+// numeroTres -= 2
+// numeroTres *= 2
+// numeroTres /= 2
+
+numeroTres *= 2 /* Ele mesmo vezes 2, operador sempre antes. */
+console.log(numeroTres)
