055e479169ac8b6fc3efda296b9de901009ba49a
unknown <vitorfaeulrich0@gmail.com>
2026-04-14 12:34:12 -0300

    Destructuring de arrays e objetos

--- destructuring.js
+++ destructuring.js
@@ -1,3 +1,6 @@
+//DESTRUCTURING:
+// Desestruturacao de array e objetos
+
 const pessoa = {
     nome: 'Vitor',
     idade: 21,
@@ -16,8 +19,35 @@ console.log(idade)
 
 //EX de usabilidade:
 
-function saudacao(pessoa){
-    console.log('olá,', pessoa.nome)
+// Assim funciona, mas tem manera melhor
+// function saudacao(pessoa){
+//     console.log('olá,', pessoa.nome)
+// }
+
+// saudacao(pessoa)
+
+// Maneira ideal
+// Recebe um objeto como parametro, mas so vamos utilizar o atributo NOME do objeto passado
+function saudacao({nome}){
+    console.log('olá,', nome)
 }
 
 saudacao(pessoa)
+
+// Pegando nome e idade de um objeto
+function saudacaoEIdade({nome, idade}){
+    console.log(`Ola ${nome}, voce tem ${idade} anos de idade`)
+}
+
+saudacaoEIdade(pessoa)
+
+//--------------------------//
+// TESTANDO COM ARRAY
+
+const frutas = ['Uva', 'Banana']
+
+const [primeira, segunda] = frutas
+
+// O que esta em cima seria a mesma coisa que o de baixo
+// const primeira = frutas[0]
+// const segunda = frutas[1]
